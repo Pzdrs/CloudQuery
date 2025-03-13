4eaf478efa693ba2b47b9555950a014b5f0024cf
Petr (Laptop) <petrbohac3@seznam.cz>
2025-03-13 14:44:33 +0100

    Initial commit

---
+++ server/src/main/java/cz/pycrs/cloudquery/CloudQueryApplication.java
@@ -0,0 +1,13 @@
+package cz.pycrs.cloudquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudQueryApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/cz/pycrs/cloudquery/CloudQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.pycrs.cloudquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
