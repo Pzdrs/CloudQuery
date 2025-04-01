d4e92fee908eb72151958c8e7af670feddbd2674
Petr (Laptop) <petrbohac3@seznam.cz>
2025-04-01 23:21:07 +0200

    env vars

--- server/src/main/java/cz/pycrs/cloudquery/CloudQueryApplication.java
+++ server/src/main/java/cz/pycrs/cloudquery/CloudQueryApplication.java
@@ -1,23 +1,12 @@
 package cz.pycrs.cloudquery;
 
-import cz.pycrs.cloudquery.entity.Country;
-import cz.pycrs.cloudquery.repository.CountryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudQueryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudQueryApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner init(CountryRepository countryRepository) {
-        return args -> {
-          countryRepository.save(new Country("CZ", "Czech Republic"));
-        };
     }
 }
