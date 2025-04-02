5ea5452cb4b9b233e4f9b951a5ec1bb45eb39bd1
Petr (Laptop) <petrbohac3@seznam.cz>
2025-04-02 14:00:59 +0200

    generate endpoint hehe

--- server/src/main/java/cz/pycrs/cloudquery/controller/WeatherController.java
+++ server/src/main/java/cz/pycrs/cloudquery/controller/WeatherController.java
@@ -4,16 +4,14 @@ import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
 import com.github.prominence.openweathermap.api.model.Coordinate;
 import cz.pycrs.cloudquery.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/weather")
 @RequiredArgsConstructor
 public class WeatherController {
     private final OpenWeatherMapClient owmClient;
+    private final WeatherService weatherService;
 
     @GetMapping("/current")
     public String currentWeather(
@@ -24,9 +22,18 @@ public class WeatherController {
         if (city != null) {
             return owmClient.currentWeather().single().byCityName(city).retrieve().asJSON();
         } else if (latitude != null && longitude != null) {
-            return owmClient.currentWeather().single().byCoordinate(Coordinate.of(latitude, longitude)).retrieve().asJSON();
+            return owmClient.oneCall3().historical().byCoordinateAndTimestamp(Coordinate.of(latitude, longitude), 1620000000).retrieve().asJSON();
+            //return owmClient.currentWeather().single().byCoordinate(Coordinate.of(latitude, longitude)).retrieve().asJSON();
         } else {
             throw new IllegalArgumentException("Either city or latitude and longitude must be provided.");
         }
     }
+
+    @PostMapping("/gen-sample")
+    public String generateSampleData(
+            @RequestParam int n
+    ) {
+        weatherService.generateSampleData(n);
+        return "ok";
+     }
 }
--- server/src/main/java/cz/pycrs/cloudquery/service/impl/WeatherServiceImpl.java
+++ server/src/main/java/cz/pycrs/cloudquery/service/impl/WeatherServiceImpl.java
@@ -6,10 +6,17 @@ import cz.pycrs.cloudquery.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 @RequiredArgsConstructor
 public class WeatherServiceImpl implements WeatherService {
-
+    private static final Logger logger = Logger.getLogger(WeatherServiceImpl.class.getName());
     private final OpenWeatherMapClient owmClient;
     private final OWMResponseRepository responseRepository;
+
+    @Override
+    public void generateSampleData(int n) {
+        logger.info("Generating " + n + " sample weather data records.");
+    }
 }
