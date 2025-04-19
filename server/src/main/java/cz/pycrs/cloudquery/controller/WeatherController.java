91b992e74984252b10856523c9088bca4ed08691
Petr (Desktop) <petrbohac3@seznam.cz>
2025-04-19 13:02:36 +0200

    fuck gradle

--- server/src/main/java/cz/pycrs/cloudquery/controller/WeatherController.java
+++ server/src/main/java/cz/pycrs/cloudquery/controller/WeatherController.java
@@ -5,10 +5,6 @@ import com.github.prominence.openweathermap.api.model.Coordinate;
 import cz.pycrs.cloudquery.entity.Measurement;
 import cz.pycrs.cloudquery.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- server/src/main/java/cz/pycrs/cloudquery/entity/Measurement.java
+++ server/src/main/java/cz/pycrs/cloudquery/entity/Measurement.java
@@ -1,10 +1,7 @@
 package cz.pycrs.cloudquery.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.Instant;
